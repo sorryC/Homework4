20eb0d1913149889ffba042343d9f64cc977db4a
Vakulenko <og4youforever@gmail.com>
2022-08-24 16:07:49 +0300

    Lesson 4

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //Lesson 3
-        int year = 2022;
-        if (year % 4 == 0 && 100 != 0 || year % 400 == 0) {
-        System.out.println(year + " является высокостным");
-        } else {
-            System.out.println(year + " не является высокостным");
+        //Lesson 4
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        if (deliveryDistance >= 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance >= 60) {
+            deliveryDays++;
+        }
+        System.out.println(" Потребуется дней " + deliveryDays);
         }
     }
-}
-
-
-
-
-
