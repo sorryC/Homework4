d4cf6ba5054eddc360b7c2de3347bd8a95bdb35d
Vakulenko <og4youforever@gmail.com>
2022-08-24 15:57:38 +0300

    Lesson 3

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //Lesson 2
-        int clientOS = 1;
-        int clientDeviceYears = 2016;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+        //Lesson 3
+        int year = 2022;
+        if (year % 4 == 0 && 100 != 0 || year % 400 == 0) {
+        System.out.println(year + " является высокостным");
         } else {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        if (clientDeviceYears < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            System.out.println(year + " не является высокостным");
         }
     }
 }
